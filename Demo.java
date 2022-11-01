546fe5a7ee5b83163869a33796d95d65b3405541
jjaimem0 <jjaimem0@email.cpcc.edu>
2022-11-01 16:03:12 -0400

    Modified
    Modified to read input from a file

--- Demo.java
+++ Demo.java
@@ -1,10 +1,12 @@
 import java.util.Scanner; // Needed in order to be able to use the Scanner class to get the user's input
+import java.io.*;
+import java.util.ArrayList;
 
 public class Demo
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-      Scanner key = new Scanner(System.in); //create a Scanner object to get the user's input
+      //Scanner key = new Scanner(System.in); //create a Scanner object to get the user's input
       
       //variables
       String poliNumber;
@@ -15,46 +17,53 @@ public class Demo
       String smokingStatus;
       double poliHoldHeight;
       double poliHoldWeight;
-                  
-      System.out.print("Please enter the Policy Number: ");
-      poliNumber = key.nextLine();
       
-      System.out.print("Please enter the Provider Name: ");
-      providerName = key.nextLine();
+      File file = new File("PolicyInformation.txt");
+      if(!file.exists())
+      {
+         System.out.print("the File does not exist");
+         System.exit(0);
+      }
       
-      System.out.print("Please enter the Policyholder's First Name: ");
-      poliHoldName = key.nextLine();
+      ArrayList<Policy> policyList = new ArrayList<>();
       
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      poliHoldLastName = key.nextLine();
+      Scanner inputFile = new Scanner(file);
+      while(inputFile.hasNext())
+      {
+         poliNumber = inputFile.nextLine();
+         providerName = inputFile.nextLine();
+         poliHoldName = inputFile.nextLine();
+         poliHoldLastName = inputFile.nextLine();
+         poliHoldAge = inputFile.nextInt();
+         inputFile.nextLine();
+         smokingStatus = inputFile.nextLine();
+         poliHoldHeight = inputFile.nextDouble();
+         poliHoldWeight = inputFile.nextDouble();
       
-      System.out.print("Please enter the Policyholder's Age: ");
-      poliHoldAge = key.nextInt();
-      
-      key.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smokingStatus = key.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      poliHoldHeight = key.nextDouble();
-      
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      poliHoldWeight = key.nextDouble();
-      
-      Policy pHolder = new Policy(poliNumber, providerName, poliHoldName, poliHoldLastName, poliHoldAge, smokingStatus, poliHoldHeight, poliHoldWeight);
-                  
-      System.out.println();
-      
-      System.out.println("Policy Number: " + pHolder.getPolicyNumber());
-      System.out.println("Provider Name: " + pHolder.getProviderName());
-      System.out.println("PolicyHolder's First Name: " + pHolder.getPoliHoldName());
-      System.out.println("PolicyHolder's Last Name: " + pHolder.getPoliHoldLastName());
-      System.out.println("PolicyHolder's Age: " + pHolder.getPoliHoldAge());
-      System.out.println("PolicyHolder's Smoking Status: " + pHolder.getPoliHoldSmokingStatus());
-      System.out.println("PolicyHolder's Height: " + pHolder.getPoliHoldHeight());
-      System.out.println("PolicyHolder's Weight: " + pHolder.getPoliHoldWeight());
-      System.out.printf("Policyholder's BMI: %.2f\n", pHolder.getBMI());
-      System.out.printf("Policy Price: $%.2f\n", pHolder.getPrice());
+         Policy pHolder = new Policy(poliNumber, providerName, poliHoldName, poliHoldLastName, poliHoldAge, smokingStatus, poliHoldHeight, poliHoldWeight);
+                 
+         System.out.println();
+         
+         policyList.add(pHolder);
+         
+         for (int i=0; i <policyList.size(); i++);
+         {
+            System.out.println("Policy Number: " + pHolder.getPolicyNumber());
+            System.out.println("Provider Name: " + pHolder.getProviderName());
+            System.out.println("PolicyHolder's First Name: " + pHolder.getPoliHoldName());
+            System.out.println("PolicyHolder's Last Name: " + pHolder.getPoliHoldLastName());
+            System.out.println("PolicyHolder's Age: " + pHolder.getPoliHoldAge());
+            System.out.println("PolicyHolder's Smoking Status: " + pHolder.getPoliHoldSmokingStatus());
+            System.out.println("PolicyHolder's Height: " + pHolder.getPoliHoldHeight() + " inches");
+            System.out.println("PolicyHolder's Weight: " + pHolder.getPoliHoldWeight() + " pounds");
+            System.out.printf("Policyholder's BMI: %.2f\n", pHolder.getBMI());
+            System.out.printf("Policy Price: $%.2f\n", pHolder.getPrice());
+         }
+      if (inputFile.hasNext()) {
+    inputFile.nextLine();
+    inputFile.nextLine();
+}
+      }
+      inputFile.close();
    }// main ends
 }// public class Demo ends
